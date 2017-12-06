7f5aead49e264aa667cdfc0faa38ce03d595bd6f
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2017-12-06 00:13:50 -0300

    working but still needs improvment

--- C/server.c
+++ C/server.c
@@ -3,17 +3,21 @@
 
 int main(){
     
-    int new_socket, bytes_read;
+    int new_socket, bytes_read, bytes_sent;
     char buffer[size] = {0};
     new_socket = listen_forever();
+
     while(1){
+        fgets(buffer, 100, stdin);
+        bytes_sent = send(new_socket, buffer, size, 0);
         bytes_read = recv( new_socket , buffer, size, 0);
         if(bytes_read == 0 || bytes_read == -1){
             printf("Client Disconnected\n");
             exit(-1);
+        }else{
+            printf("%s\n", buffer );
+            memset(buffer , 0, sizeof(buffer));
         }
-        printf("%d - %s\n", bytes_read, buffer );
-        memset(buffer , 0, sizeof(buffer));
     }
     return 0;
 }
