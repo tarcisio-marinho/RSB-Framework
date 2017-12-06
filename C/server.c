2943c846f5fe3585c5c64cb66b28b64fddd1f127
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2017-12-06 00:19:18 -0300

    if server fall, client reconnect

--- C/server.c
+++ C/server.c
@@ -5,18 +5,20 @@ int main(){
     
     int new_socket, bytes_read, bytes_sent;
     char buffer[size] = {0};
+    char output[MAX_TERMINAL_OUTPUT] = {0};
     new_socket = listen_forever();
 
     while(1){
+        printf(">>> ");
         fgets(buffer, 100, stdin);
         bytes_sent = send(new_socket, buffer, size, 0);
-        bytes_read = recv( new_socket , buffer, size, 0);
+        bytes_read = recv( new_socket , output, MAX_TERMINAL_OUTPUT, 0);
         if(bytes_read == 0 || bytes_read == -1){
             printf("Client Disconnected\n");
             exit(-1);
         }else{
-            printf("%s\n", buffer );
-            memset(buffer , 0, sizeof(buffer));
+            printf("%s\n", output );
+            memset(output , 0, sizeof(output));
         }
     }
     return 0;
